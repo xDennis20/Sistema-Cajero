2be9bbf525094b39de477ed3d0e2eca187f02957
Dennis <dennis_6@outlook.com>
2025-08-06 21:08:55 -0500

    Agregue la funcion agregar_pedidos

--- Mesa.py
+++ Mesa.py
@@ -4,3 +4,11 @@ class Mesa:
     numero: int
     pedidos: list[dict] = field(default_factory=list)
     total: float = 0.0
+    
+    def agregar_pedido(self, nombre: str, precio: float):
+        self.pedidos.append({"Comida": nombre,"Precio": precio})
+        self.total += precio
+        print(self.pedidos)
+
+prueba = Mesa(1)
+prueba.agregar_pedido("Arro con pollo",2.50)
