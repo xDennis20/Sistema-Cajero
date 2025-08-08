41a5b10ad2578de254e77968a91f0e160958e053
Dennis <dennis_6@outlook.com>
2025-08-08 15:56:08 -0500

    Agregue funcion de resumen del dia y hice algunos cambios en algunas funciones

--- Cajero.py
+++ Cajero.py
@@ -13,33 +13,25 @@ class Cajero:
         self.mesas[numero] = Mesa(numero)
         print(f"Mesa {numero} fue creada con exito")
     
-    def registrar_pedido_en_mesa(self,numero_mesa: int):
+    def registrar_pedido_en_mesa(self,numero_mesa: int, comida: str, precio: float):
         if numero_mesa in self.mesas:
-            comida = input("Ingrese el nombre de la comida o bebida: ")
-            precio = float(input("Ingrese el precio de la comida o bebida: "))
             self.mesas[numero_mesa].agregar_pedido(comida,precio)
             print(f"El pedido fue registrado correctamente en la mesa {numero_mesa}")
         else:
             print("La mesa que intento registrar el pedido no existe.")
 
-    def cobrar_mesa(self,numero_mesa: int):
+    def cobrar_mesa(self,numero_mesa: int,dinero_cliente: float):
         if numero_mesa in self.mesas:
             mesa = self.mesas[numero_mesa]
-            dinero_cliente = float(input("Ingrese el dinero que dio el cliente: "))
             if dinero_cliente < mesa.total:
                 print(f"Falta ${(mesa.total - dinero_cliente):.2f} para poder cubrir el total.")
                 return
-            vuelto = mesa.total - dinero_cliente
+            vuelto = dinero_cliente - mesa.total
             print(f"Vuelto a dar al cliente es: {vuelto:.2f}")
         else:
             print("La mesa que ingreso no existe")
 
     def resumen_dia(self):
-
-        pass
-
-prueba = Cajero()
-prueba.crear_mesa(3)
-prueba.registrar_pedido_en_mesa(3)
-prueba.registrar_pedido_en_mesa(3)
-prueba.cobrar_mesa(3)
+        print("*** RESUMEN DEL DIA ***")
+        for key,valor in self.mesas.items():
+            print(f"La Mesa {key} genero un total de ${valor.total:.2f}")
